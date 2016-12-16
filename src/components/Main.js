88cb1e589af6a425e7405fee1fd14410099cdc28
sdshaoda <sdshaoda@126.com>
2016-12-16 17:33:58 +0800

    图片组件构建一

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,23 +6,35 @@ import React from 'react';
 
 // 获取图片相关的数据
 let imageDatas = require('../data/imageDatas.json');
-let yeomanImage = require('../images/yeoman.png');
+// let yeomanImage = require('../images/yeoman.png');
 
-// 利用自执行函数，将图片名信息转换成图片URL路径信息
-imageDatas = (function (imageDatasArr) {
-  for (let i = 0, l = imageDatasArr.length; i < l; i++) {
-    let singleImageData = imageDatasArr[i];
+// 将图片名信息转换成图片URL路径信息
+imageDatas.map((item, index) => {
+  imageDatas[index].imageURL = require('../images/' + imageDatas[index].fileName);
+});
 
-    singleImageData.imageURL = require('../images/' + singleImageData.fileName);
-
-    imageDatasArr[i] = singleImageData;
-
-    return imageDatasArr;
+class ImgFigure extends React.Component {
+  render() {
+    return (
+      <figure className="img-figure">
+        <img src={this.props.data.imageURL} alt={this.props.data.title} />
+        <figcaption>
+          <h2 className="img-title">{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+    );
   }
-})(imageDatas);
+}
 
 class AppComponent extends React.Component {
   render() {
+    let controllerUnits = [];
+    let imgFigures = [];
+
+    imageDatas.forEach(function (item, index) {
+      imgFigures.push(<ImgFigure data={item} key={index} />);
+    });
+
     return (
       <div>
         {/*<div className="index">
@@ -30,9 +42,11 @@ class AppComponent extends React.Component {
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
       </div>*/}
         <section className="stage">
-          <section className="img-sec"></section>
+          <section className="img-sec">
+            {imgFigures}
+          </section>
           <nav className="controller-nav">
-            shaoda
+            {controllerUnits}
           </nav>
         </section>
       </div>
