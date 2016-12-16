8eb1d50804af77d8bbd1be9a8ccfe8eea0233c1a
sdshaoda <sdshaoda@126.com>
2016-12-17 00:20:56 +0800

    finish gallery-by-react

--- src/components/Main.js
+++ src/components/Main.js
@@ -76,6 +76,37 @@ class ImgFigure extends React.Component {
   }
 }
 
+// 控制组件
+class ControllerUnit extends React.Component {
+  handleClick(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (this.props.arrange.isCenter) {
+      this.props.inverse();
+    } else {
+      this.props.center();
+    }
+  }
+
+  render() {
+    let controllerUnitClassName = 'controller-unit';
+
+    // 如果对应的是居中图片
+    if (this.props.arrange.isCenter) {
+      controllerUnitClassName += ' is-center';
+      // 同时图片是翻转的
+      if (this.props.arrange.isInverse) {
+        controllerUnitClassName += ' is-inverse';
+      }
+    }
+
+    return (
+      <span className={controllerUnitClassName} onClick={this.handleClick.bind(this)}></span>
+    );
+  }
+}
+
 class AppComponent extends React.Component {
   constructor(props) {
     super(props);
@@ -269,7 +300,23 @@ class AppComponent extends React.Component {
     let imgFigures = [];
 
     imageDatas.map((item, index) => {
-      imgFigures.push(<ImgFigure ref={'imgFigure' + index} data={item} key={index} arrange={this.state.imgsArrangeArr[index]} inverse={this.inverse.bind(this, index)} center={this.center.bind(this, index)} />);
+      imgFigures.push(
+        <ImgFigure
+          ref={'imgFigure' + index}
+          data={item}
+          key={index}
+          arrange={this.state.imgsArrangeArr[index]}
+          inverse={this.inverse.bind(this, index)}
+          center={this.center.bind(this, index)} />
+      );
+
+      controllerUnits.push(
+        <ControllerUnit
+          key={index}
+          arrange={this.state.imgsArrangeArr[index]}
+          inverse={this.inverse.bind(this, index)}
+          center={this.center.bind(this, index)} />
+      );
     });
 
     return (
